43bc0ff34089f7f5c1379ee0259f8ce9cec2bfd1
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-04-18 23:42:40 +0200

    Initial Commit

---
+++ Habitude/HabitudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitudeApp.swift
+//  Habitude
+//
+//  Created by Philip Andersson on 2023-04-18.
+//
+
+import SwiftUI
+
+@main
+struct HabitudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
